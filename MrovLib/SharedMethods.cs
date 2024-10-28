66880b10856dd4d2b670e851623115046a0edfce
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-10-28 17:50:01 +0100

    i'll hopefully get rid of oldLLL soon

--- MrovLib/SharedMethods.cs
+++ MrovLib/SharedMethods.cs
@@ -11,125 +11,42 @@ namespace MrovLib.API
 	{
 		public static string GetWeather(SelectableLevel level)
 		{
-			string weather;
-
-			if (Plugin.WeatherTweaks.IsModPresent)
-			{
-				weather = WeatherTweaks.CurrentWeather(level);
-			}
-			else if (Plugin.LLL.IsModPresent)
-			{
-				weather = LLL.GetWeather(level);
-			}
-			else
-			{
-				weather = level.currentWeather.ToString();
-			}
-
-			Plugin.LogDebug($"Weather: {weather}");
-
-			return weather == "None" ? "" : weather;
+			return MrovLib.SharedMethods.GetWeather(level);
 		}
 
 		public static string GetNumberlessPlanetName(SelectableLevel level)
 		{
-			return StringResolver.GetNumberlessName(level);
+			return MrovLib.SharedMethods.GetNumberlessPlanetName(level);
 		}
 
 		public static List<GrabbableObject> GetShipObjects()
 		{
-			GameObject ship = GameObject.Find("/Environment/HangarShip");
-			return ship.GetComponentsInChildren<GrabbableObject>().ToList();
+			return MrovLib.SharedMethods.GetShipObjects();
 		}
 
 		public static List<SelectableLevel> GetGameLevels()
 		{
-			// Plugin.logger.LogWarning("GetGameLevels called");
-
-			if (Plugin.LLL.IsModPresent)
-			{
-				// Plugin.LogDebug($"LLL present");
-				return LLL.GetLevels();
-			}
-			else if (LLLOldPlugin.IsTheOldLLLActive())
-			{
-				// Plugin.LogDebug($"LLLOld present");
-				return LLLOldPlugin.GetSelectableLevels();
-			}
-			else
-			{
-				// Plugin.LogDebug($"NOLLL present");
-				return StartOfRound.Instance.levels.ToList();
-			}
+			return MrovLib.SharedMethods.GetGameLevels();
 		}
 
 		public static bool IsMoonHiddenLLL(SelectableLevel level)
 		{
-			if (Plugin.LLL.IsModPresent)
-			{
-				return LLL.IsMoonHidden(level);
-			}
-			else if (LLLOldPlugin.IsTheOldLLLActive())
-			{
-				return LLLOldPlugin.IsMoonHidden(level);
-			}
-
-			return false;
+			return MrovLib.SharedMethods.IsMoonHiddenLLL(level);
 		}
 
 		public static bool IsMoonLockedLLL(SelectableLevel level)
 		{
-			if (Plugin.LLL.IsModPresent)
-			{
-				return LLL.IsMooonLocked(level);
-			}
-			else if (LLLOldPlugin.IsTheOldLLLActive())
-			{
-				return LLLOldPlugin.IsMoonLocked(level);
-			}
-
-			return false;
+			return MrovLib.SharedMethods.IsMoonLockedLLL(level);
 		}
 
 		public static List<TerminalNode> GetLevelTerminalNodes(SelectableLevel level)
 		{
-			if (Plugin.LLL.IsModPresent)
-			{
-				return LLL.GetLevelTerminalNodes(level);
-			}
-			else
-			{
-				return [];
-			}
+			return MrovLib.SharedMethods.GetLevelTerminalNodes(level);
 		}
 
 		public static object GetLLLMoonsCataloguePage()
 		{
-			if (Plugin.LLL.IsModPresent)
-			{
-				return LLL.GetMoonsCataloguePage();
-			}
-			else if (LLLOldPlugin.IsTheOldLLLActive())
-			{
-				return LLLOldPlugin.GetMoonsCataloguePage();
-			}
-
-			return null;
+			return MrovLib.SharedMethods.GetLLLMoonsCataloguePage();
 		}
-
-		// public static int GetPrice(int beforeDiscountPrice)
-		// {
-		//     Plugin.logger.LogWarning($"price: {beforeDiscountPrice}");
-
-		//     if (Plugin.isLGUPresent)
-		//     {
-		//         Plugin.logger.LogInfo($"LGU is present");
-		//         return LategameUpgradesCompatibility.GetMoonPrice(beforeDiscountPrice);
-		//     }
-		//     else
-		//     {
-		//         return beforeDiscountPrice;
-		//     }
-		// }
 	}
 }
