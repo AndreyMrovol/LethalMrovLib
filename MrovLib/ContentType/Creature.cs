8d53375a0f16f40987a44f23b820c251d75215a3
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-01-09 17:24:41 +0100

    Update Creature.cs

--- MrovLib/ContentType/Creature.cs
+++ MrovLib/ContentType/Creature.cs
@@ -4,6 +4,7 @@ namespace MrovLib.ContentType
 	{
 		public string Name { get; set; }
 		public TerminalNode InfoNode { get; set; }
+		public bool Discovered => ContentManager.Terminal.scannedEnemyIDs.Contains(InfoNode.creatureFileID);
 
 		public override string ToString()
 		{
