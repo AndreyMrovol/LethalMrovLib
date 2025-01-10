d8d8a54a162b051a992d20cac59bbd438cb8a52a
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-01-10 09:40:59 +0100

    Update MapperRestore.cs

--- MrovLib/Compatibility/MapperRestore.cs
+++ MrovLib/Compatibility/MapperRestore.cs
@@ -1,9 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using LethalLevelLoader;
-
 namespace MrovLib.Compatibility
 {
 	public class MapperRestoreCompat(string guid, string version = null) : CompatibilityBase(guid, version) { }
