26db6add35ffb53b382b5bac48c69f87b078f5dd
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-04-08 19:37:08 +0200

    make price dynamically read

--- MrovLib/ContentType/BuyableCar.cs
+++ MrovLib/ContentType/BuyableCar.cs
@@ -4,13 +4,15 @@ namespace MrovLib.ContentType
 	{
 		public BuyableVehicle Vehicle;
 
+		public override int Price => Nodes.Node.itemCost;
+
 		public BuyableCar(Terminal terminal, RelatedNodes nodes)
 			: base(terminal, nodes)
 		{
 			Type = PurchaseType.Vehicle;
 
 			Vehicle = ContentManager.Terminal.buyableVehicles[nodes.Node.buyVehicleIndex];
-			Price = Nodes.Node.itemCost;
+			// Price = Nodes.Node.itemCost;
 			Name = Vehicle.vehicleDisplayName;
 
 			ContentManager.Vehicles.Add(this);
