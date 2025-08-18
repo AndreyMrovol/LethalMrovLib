a00ad8181e4f07b0ca2fcde75bd241ebe5a485c3
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-08-18 20:35:22 +0200

    Update Logger.cs

--- MrovLib/Logger.cs
+++ MrovLib/Logger.cs
@@ -1,48 +1,98 @@
+using System;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 
 namespace MrovLib
 {
-	public class Logger(string name, ConfigEntry<bool> enabled = null)
+	public enum LoggingType
 	{
-		private ManualLogSource LogSource = BepInEx.Logging.Logger.CreateLogSource(name);
-		private ConfigEntry<bool> ConfigEntry = enabled ?? Plugin.DebugLogging;
+		Basic,
+		Debug,
+		Developer,
+	}
 
-		// this is overengineered to fuck
-		public void Log(LogLevel level, object data)
+	public class Logger
+	{
+		public virtual string ModName => "MrovLib";
+		private string _name;
+		private ManualLogSource _logSource = BepInEx.Logging.Logger.CreateLogSource($"MrovLib");
+
+		private LoggingType _defaultLoggingType;
+
+		public Logger(string SourceName, LoggingType defaultLoggingType = LoggingType.Basic)
 		{
-			if (ConfigEntry.Value)
+			_defaultLoggingType = defaultLoggingType;
+			_name = SourceName == ModName ? "" : SourceName;
+		}
+
+		[Obsolete("Use Logger(string SourceName, LoggingType defaultLoggingType) instead.")]
+		public Logger(string name)
+		{
+			_defaultLoggingType = LoggingType.Debug;
+			_name = name == ModName ? "" : name;
+		}
+
+		[Obsolete("Use Logger(string SourceName, LoggingType defaultLoggingType) instead.")]
+		public Logger(string name, ConfigEntry<bool> enabled = null)
+		{
+			_defaultLoggingType = LoggingType.Developer;
+			_name = name == ModName ? "" : name;
+		}
+
+		[Obsolete("Use Logger(string SourceName, LoggingType defaultLoggingType) instead.")]
+		public Logger(string name, ConfigEntry<LoggingType> defaultType)
+		{
+			_defaultLoggingType = defaultType.Value;
+			_name = name == ModName ? "" : name;
+		}
+
+		public virtual bool ShouldLog(LoggingType type)
+		{
+			return LocalConfigManager.Debug.Value >= type;
+		}
+
+		public void LogCustom(string data, LogLevel level, LoggingType type)
+		{
+			if (ShouldLog(type))
 			{
-				LogSource.Log(level, data);
+				_logSource.Log(level, $"[{_name}] {data}");
 			}
 		}
 
-		public void LogInfo(object data)
+		public void Log(LogLevel level, string data)
+		{
+			if (ShouldLog(_defaultLoggingType))
+			{
+				_logSource.Log(level, $"[{_name}] {data}");
+			}
+		}
+
+		public void LogInfo(string data)
 		{
 			Log(LogLevel.Info, data);
 		}
 
-		public void LogWarning(object data)
+		public void LogWarning(string data)
 		{
 			Log(LogLevel.Warning, data);
 		}
 
-		public void LogError(object data)
+		public void LogError(string data)
 		{
 			Log(LogLevel.Error, data);
 		}
 
-		public void LogDebug(object data)
+		public void LogDebug(string data)
 		{
 			Log(LogLevel.Debug, data);
 		}
 
-		public void LogFatal(object data)
+		public void LogFatal(string data)
 		{
 			Log(LogLevel.Fatal, data);
 		}
 
-		public void LogMessage(object data)
+		public void LogMessage(string data)
 		{
 			Log(LogLevel.Message, data);
 		}
