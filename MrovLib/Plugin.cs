76c16833a413da0491a55625381eec4560c79f62
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-06-03 23:27:05 +0200

    Update Plugin.cs

--- MrovLib/Plugin.cs
+++ MrovLib/Plugin.cs
@@ -7,20 +7,20 @@ using UnityEngine.SceneManagement;
 
 namespace MrovLib
 {
-	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 	[BepInDependency("imabatby.lethallevelloader", BepInDependency.DependencyFlags.SoftDependency)]
 	[BepInDependency("TeamXiaolan.DawnLib", BepInDependency.DependencyFlags.SoftDependency)]
 	public class Plugin : BaseUnityPlugin
 	{
 		internal static ManualLogSource logger;
 		internal static Logger DebugLogger;
-		internal static Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+		internal static Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
 
 		internal static ConfigEntry<LoggingType> DebugLogging;
 
 		public static LLL LLL;
 		public static DawnLib DawnLibCompat;
-		public static WeatherTweaks WeatherTweaks;
+		public static WeatherTweaksCompat WeatherTweaks;
 		public static MapperRestoreCompat MapperRestoreCompat;
 		public static ShipInventoryCompat ShipInventoryCompat;
 		public static ItemWeightsCompat ItemWeightsCompat;
@@ -37,7 +37,7 @@ namespace MrovLib
 			DebugLogger = new("Debug", LoggingType.Debug);
 
 			// Plugin startup logic
-			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
 			DebugLogger.LogWarning($"Debug logs enabled!");
 
