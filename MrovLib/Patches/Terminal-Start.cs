42016ddd05beb24ae5dcdd98968ee90523cf6be0
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-06-22 21:52:09 +0200

    Update Terminal-Start.cs

--- MrovLib/Patches/Terminal-Start.cs
+++ MrovLib/Patches/Terminal-Start.cs
@@ -9,7 +9,9 @@ namespace MrovLib.Patches
 		[HarmonyPostfix]
 		private static void RunMeFirst(Terminal __instance)
 		{
+			EventManager.ContentManagerReady.AddListener(LevelHelper.ParseVanillaMoonOrder);
 			LevelHelper.Populate();
+
 			ContentManager.Init(__instance);
 		}
 
